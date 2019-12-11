e430a9e2a18a3bb59b63f1de3c725557c0f591f0
thekernelcode <45094334+thekernelcode@users.noreply.github.com>
2019-12-11 19:02:24 +0000

    Update Worldmap.cs

--- Assets/Worldmap.cs
+++ Assets/Worldmap.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Worldmap : MonoBehaviour {
 
     public int mapWidth = 10;  
     public int mapHeight = 10;
     public GameObject tilePrefab;
+
     public GameObject[,] tiles;
     public List<GameObject> tileGameObjects;
 
@@ -28,6 +30,7 @@ public class Worldmap : MonoBehaviour {
                 go.name = (x + " , " + y);
                 go.transform.SetParent(this.transform);
                 tileGameObjects.Add(go);
+                tiles[x, y] = go;
             }
         }
 
@@ -44,6 +47,7 @@ public class Worldmap : MonoBehaviour {
         {
             CrimeInProgress();
         }
+
 	}
 
     public Tile GetTileAt (int x, int y)
