a9a3baef7206a431d75f04e3f074a58247fa739b
TheKernelCode <thekernelcode@gmail.com>
2019-12-11 15:20:54 +0000

    First Commit
    Basic World gen

---
+++ Assets/Tile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Worldmap tileMapOfWorld;
+
+    public int xPos;
+    public int yPos;
+
+    public Material defaultMaterial;
+    public Material crimeInProgressMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Make tile aware if the world it is in
+        tileMapOfWorld = FindObjectOfType<Worldmap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/Worldmap.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Worldmap : MonoBehaviour {
+
+    public int mapWidth = 10;  
+    public int mapHeight = 10;
+    public GameObject tilePrefab;
+    public GameObject[,] tiles;
+
+	// Use this for initialization
+	void Start () {
+
+        tiles = new GameObject[mapWidth, mapHeight];
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                GameObject go = Instantiate(tilePrefab, new Vector3(x, 0, y), Quaternion.identity);
+                go.GetComponent<Tile>().xPos = x;       
+                go.GetComponent<Tile>().yPos = y;       
+                go.name = (x + " , " + y);
+                go.transform.SetParent(this.transform);
+            }
+        }
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public Tile GetTileAt (int x, int y)
+    {
+        GameObject go = tiles[x, y];
+        Tile t = go.GetComponent<Tile>();
+        return t;
+    }
+}
